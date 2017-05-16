794e56c989d1f58a58929f391b885945f12d9e80
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-05-16 04:06:38 +0100

    get_me function implemented

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,13 @@
 class Kele
   require 'httparty'
+  require 'json'
   include HTTParty
 
-  attr_reader :token, :response
+
+  def get_me
+    response = self.class.get("/users/me", { "headers": { "authorization" => @auth_token }})
+    JSON.parse(response.body)
+  end
 
   def initialize(email, password)
       @base_uri = "https://www.bloc.io/api/v1"
@@ -11,12 +16,12 @@ class Kele
         "email": email,
         "password": password
       }
-      @response = self.class.post("/sessions", { "body": body})
+      response = self.class.post("/sessions", { "body": body})
 
-      raise @response.message or 'Login Failed' if @response.code == 401
-      raise @response.message or 'Page not found' if @response.code == 404
-      raise @response.message or 'Connection failed' unless @response.code == 200
-      
-      @token = @response.parsed_response["auth_token"]
+      raise response.message or 'Login Failed' if response.code == 401
+      raise response.message or 'Page not found' if response.code == 404
+      raise response.message or 'Connection failed' unless response.code == 200
+
+      @auth_token = response.parsed_response["auth_token"]
     end
 end
